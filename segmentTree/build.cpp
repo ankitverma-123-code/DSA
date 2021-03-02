ac6c3a0ba0ca0d925190435cdd9e06061796cc12
ankitverma-123-code <av4135850.19je0144@cse.iitism.ac.in>
2021-03-02 18:56:32 +0530

    added segment tree

--- segmentTree/build.cpp
+++ segmentTree/build.cpp
@@ -5,7 +5,6 @@ void buildTree(int* a,int* tree,int start,int end,int nodeidx){
         tree[nodeidx]=a[start];
         return;
     }
-
     int mid=(start+end)/2;
 
     buildTree(a,tree,start,mid,2*nodeidx);
@@ -14,13 +13,31 @@ void buildTree(int* a,int* tree,int start,int end,int nodeidx){
     tree[nodeidx]=tree[2*nodeidx]+tree[2*nodeidx+1];
 }
 
+void updateintree(int* a,int* tree,int start,int end,int nodeidx,int idx,int value){
+
+if(start==end){
+    a[idx]=value;
+    tree[nodeidx]=value;
+    return;
+}
+    int mid =(start+end)/2;
+
+    if(idx>mid){
+        updateintree(a,tree,mid+1,end,2*nodeidx+1,idx,value);
+    }
+    else updateintree(a,tree,start,mid,2*nodeidx,idx,value);
+
+    tree[nodeidx]=tree[2*nodeidx]+tree[2*nodeidx+1];
+}
+
 
 int main(){
-    int a[]={1,2,3,4,5,6,7,8,9};
-    int* tree=new int[18];
+    int a[]={1,2,3,4,5};
+    int* tree=new int[10];
 
-    buildTree(a,tree,0,8,1);
-    for(int i=1;i<=15;i++)
+    buildTree(a,tree,0,4,1);
+    updateintree(a,tree,0,4,1,2,10);
+    for(int i=1;i<=9;i++)
      cout<<tree[i]<<endl;
 
      delete [] tree;
